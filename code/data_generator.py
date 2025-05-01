b0b2920660dcfbf3207b7a60e807b7f25c66c138
alexander <mrprofu361@gmail.com>
2025-05-01 15:18:17 +0300

    clean repository

---
+++ code/data_generator.py
@@ -0,0 +1,60 @@
+import random
+import nni
+import numpy as np
+
+import io
+import graphviz
+import matplotlib.pyplot as plt
+from PIL import Image
+
+import json
+import os
+
+from tqdm.notebook import tqdm
+
+
+DARTS_OPS = [
+    # 'none',
+    "max_pool_3x3",
+    "avg_pool_3x3",
+    "skip_connect",
+    "sep_conv_3x3",
+    "sep_conv_5x5",
+    "dil_conv_3x3",
+    "dil_conv_5x5",
+]
+
+
+def generate_cells(num_nodes, name='normal', operations=DARTS_OPS):
+    cells = dict()
+    for i in range(num_nodes - 1):
+        cur_indexes = list(range(0, i + 2))
+        random_op_0, random_op_1 = random.choices(operations, k=2)
+        
+        random_index_0, random_index_1 = random.sample(cur_indexes, k=2)
+        
+        op_str_0 = f'{name}/op_{i + 2}_0'
+        op_str_1 = f'{name}/op_{i + 2}_1'
+        input_str_0 = f'{name}/input_{i + 2}_0'
+        input_str_1 = f'{name}/input_{i + 2}_1'
+        
+        cells[op_str_0] = random_op_0
+        cells[input_str_0] = [random_index_0]
+        cells[op_str_1] = random_op_1
+        cells[input_str_1] = [random_index_1]
+        
+    return cells
+
+def generate_arch_dicts(N_MODELS):
+    arch_dicts = []
+    for _ in tqdm(range(N_MODELS)):
+        normal_cell = generate_cells(5, name='normal')
+        reduction_cell = generate_cells(5, name='reduce')
+        arch_dict = {**normal_cell, **reduction_cell}
+        arch_dicts.append(arch_dict)
+    return arch_dicts
+
+if __name__ == "__main__":
+    N_MODELS = 3
+    arch_dicts = generate_arch_dicts(N_MODELS)
+    print(arch_dicts)
