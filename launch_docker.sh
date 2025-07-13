bcc27292707e7fa404e45756f0a37b48d50a88ab
Petr Babkin <p.k.babkin@gmail.com>
2025-07-13 17:48:13 +0300

    docker configured

---
+++ launch_docker.sh
@@ -0,0 +1,36 @@
+IMAGE_NAME="surrogate-nes-image"
+
+docker build -t $IMAGE_NAME . -f Dockerfile
+
+# Установка XAUTH если не задана
+if [ -z "$XAUTH" ]; then
+    XAUTH=/tmp/.docker.xauth
+fi
+
+# Создание xauth файла если не существует
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY 2>/dev/null | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+# Запуск контейнера
+docker run \
+    -it \
+    --rm \
+    --name surrogate-nes-container \
+    --gpus all \
+    --ipc host \
+    --runtime=nvidia \
+    -p 6012:6011 \
+    -e DISPLAY=$DISPLAY \
+    --shm-size=64g \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --env LIBGL_ALWAYS_SOFTWARE=1 \
+    --volume="$XAUTH:$XAUTH" \
+    -e XAUTHORITY=$XAUTH \
+    $IMAGE_NAME /bin/bash
