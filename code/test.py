e60c8cc22f7a456f756db3b19d4f2875f9cf2ef9
Petr Babkin <p.k.babkin@gmail.com>
2025-07-21 11:39:40 +0300

    Prep for merge

--- code/test.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def binom(n, m):
-    return math.factorial(n) // (math.factorial(n - m) * math.factorial(m))
-
-result = binom(2, 2) * binom(3, 2) * binom(4, 2) * binom(5, 2) * 7 ** 8
-
-print(result // 1e9)
