8a644ac28a3f4d441856e30a5958064bc2e2e7e2
alexander <mrprofu361@gmail.com>
2026-01-25 02:32:58 +0300

    add ouput_metrics

--- code/scripts/surrogate.sh
+++ code/scripts/surrogate.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 cd "$(dirname "$0")/.."
 
 
-CONFIG="configs/surrogate_hp_CIFAR10.json"
+CONFIG="configs/surrogate_hp_CIFAR100.json"
 python train_surrogate.py --hyperparameters_json "$CONFIG"
